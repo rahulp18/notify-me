05f1c2fb486f07f96deccbd3fcddd3c5846b8bf0
rahulp18 <pradhanrahul3346@gmail.com>
2023-10-22 11:03:31 +0530

    fixed errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,9 +8,9 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody,SendEMail } from "@/lib/nodemailer";
 
 
-export const maxDuration=300;
-export const dynamic='force-dynamic'
-export const revalidate=0;
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 
 export async function GET(request:Request){
